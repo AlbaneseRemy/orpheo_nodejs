cb79830f4b07697259b7da07c3ca620dbf26347c
Rémy Albanese <remy.albanese2@gmail.com>
2024-01-10 16:44:30 +0100

    First commit

---
+++ public/scripts/app.js
@@ -0,0 +1,79 @@
+document.getElementById('generateButton').addEventListener('click', async () => {
+    const text = document.getElementById('textInput').value;
+    const logUrl = document.getElementById('urlCheckbox').checked;
+    if (text.trim() === '') {
+        alert('Please enter some text to generate a QR code.');
+        return;
+    }
+    try {
+        const response = await fetch('/generate-qrcode', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text, logUrl })
+        });
+        const qrCodeHTML = await response.text();
+        document.getElementById('qrCodeContainer').innerHTML = qrCodeHTML;
+    } catch (error) {
+        console.error('Error fetching QR code:', error);
+    }
+});
+
+
+const fileInput = document.getElementById('fileInput');
+const uploadButton = document.getElementById('uploadButton');
+
+uploadButton.addEventListener('click', () => {
+    fileInput.click();
+});
+
+fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    
+
+
+   if (file && file.name.endsWith('.json')) {
+    console.log('JSON file selected:', file.name);
+    console.log('File selected:', file.name);
+    document.getElementById('fileName').innerHTML = file.name;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        jsonInput = JSON.parse(e.target.result);
+        console.log(jsonInput);
+        console.log("Type: " + typeof jsonInput);
+    };
+    reader.readAsText(file);
+    
+} else {
+    alert('Please select a JSON file.');
+}
+
+});
+
+document.getElementById('bulkQrCodes').addEventListener('click', async () => {
+    for(keys of jsonInput.keys){
+        try {
+
+            const response = await fetch('/generate-qrcode', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ text: keys.url, logUrl: false, size: 400 })
+            });
+            const qrCodeHTML = await response.text();
+            let div = document.createElement('div');
+            let p = document.createElement('p');
+            p.innerHTML = keys.url;
+            p.classList.add('centerText');
+            div.appendChild(p);
+            div.innerHTML += qrCodeHTML;
+            document.getElementById('qrCodesContainer').appendChild(div);
+        } catch (error) {
+            console.error('Error fetching QR code:', error);
+        }
+    }
+});
+
